1a6ef71aeddcb3b3341aa7eb8ecaee9e949f41dc
SimoneMondello <mondellosimo@gmail.com>
2022-05-19 16:55:44 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PostController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore_01.Models;
+using NetCore_01.Utils;
+
+namespace NetCore_01.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            List<Post> posts = PostData.GetPosts();
+
+            return View("HomePage", posts);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Post postFound = null;
+            
+            foreach(Post post in PostData.GetPosts())
+            {
+                if(post.Id == id)
+                {
+                    postFound = post;
+                    break;
+                }
+            }
+
+            if(postFound != null)
+            {
+                return View("Details", postFound);
+            } else
+            {
+                return NotFound("Il post con id " + id + " non è stato trovato");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("FormPost");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Post nuovoPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FormPost", nuovoPost);
+            }
+
+            Post nuovoPostConId = new Post(PostData.GetPosts().Count, nuovoPost.Title, nuovoPost.Description, nuovoPost.Image);
+
+            // Il mio modello è corretto
+            PostData.GetPosts().Add(nuovoPostConId);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Utils/PostData.cs
@@ -0,0 +1,34 @@
+﻿using NetCore_01.Models;
+
+namespace NetCore_01.Utils
+{
+    public static class PostData
+    {
+
+        private static List<Post> posts;
+
+        public static List<Post> GetPosts()
+        {
+            if(PostData.posts != null)
+            {
+                return PostData.posts;
+            }
+
+            List<Post> nuovaListaPosts = new List<Post>();
+
+            for(int i=0; i<1; i++)
+            {
+                Post post = new Post(i, "Pizza: " + i, "Margherita", "https://th.bing.com/th/id/OIP.N6oVQxrI4PN_sXzUt0IY6AHaF9?w=252&h=203&c=7&r=0&o=5&pid=1.7" + i + "/300/200");
+                nuovaListaPosts.Add(post);
+                Post post1 = new Post(i, "Pizza: " + i, "Norma", "https://th.bing.com/th/id/OIP.nKZYHaqKxMfjMCNvuv22DwHaE1?w=273&h=180&c=7&r=0&o=5&pid=1.7" + i + "/300/200");
+                nuovaListaPosts.Add(post1);
+            }
+
+            PostData.posts = nuovaListaPosts;
+
+            return PostData.posts;
+
+        }
+
+    }
+}
